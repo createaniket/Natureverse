6122e5fb5f703bb13da73aceca93ef162f5b1059
Aniket Pratap Singh <103597886+createaniket@users.noreply.github.com>
2022-12-31 04:27:07 +0530

    minor bug fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,46 +1,71 @@
-import React from 'react'
-import './Footer.css'
-import {Link } from 'react-router-dom'
-import {SiNetlify} from 'react-icons/si'
-import {FaUser} from 'react-icons/fa'
-import {AiFillMail} from 'react-icons/ai'
-import {BsFillTelephoneInboundFill} from 'react-icons/bs'
+import React from "react";
+import "./Footer.css";
+import { FaUser } from "react-icons/fa";
+import { AiFillMail } from "react-icons/ai";
+import { BsFillTelephoneInboundFill } from "react-icons/bs";
 
 const Footer = () => {
+ 
     return (
-        <div id='footer' className='footer'>
-            <div className='containerfooter'>
-              
-                <div className="lefter">
+ 
+ <div id="footer" className="footer">
+      <div className="containerfooter">
+        <div className="lefter">
+          <ul className="socials">
+            <li>
+              <a
+                href="https://github.com/createaniket"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i class="fa fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/aniket-pratap-singh-28669b193"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i class="fa fa-linkedin-square"></i>
+              </a>
+            </li>
 
-                <ul className="socials">
-                    <li><Link href="#"><i class="fa fa-github"></i></Link></li>
-                    <li><Link href="#"><i class="fa fa-linkedin-square"></i></Link></li>
-
-                    <li><Link href="#"><i class="fa fa-linke-square" ><SiNetlify className='net-use' /></i></Link></li>
-                    <li><Link href="#"><i  class="fa fa-linke-square"> <FaUser className='net-use'  /></i></Link></li>
-                </ul>
-                    
-                </div>
-
-                <div className='middle'>
-
-                  <p>Copyright &copy; 2022 createaniket</p>
-                    
-                </div>
-
-
-                <div className='righter'>
-                    <div className="contactfooter">
-                      <p id='contactheadingfooter'>Contact</p>
-                      <p id='phone'><AiFillMail />  +91 6392347125 </p>
-                      <p id='mail'><BsFillTelephoneInboundFill />  aniketpratapsingh7310@gmail.com</p>
-                      </div>
-
-                </div>
-            </div>
+            <li>
+              <a
+                href="https://aniketdev-portfolio.netlify.app"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <i class="fa fa-linke-square">
+                  {" "}
+                  <FaUser className="net-use" />
+                </i>
+              </a>
+            </li>
+          </ul>
         </div>
-    )
-}
 
-export default Footer
+        <div className="middle">
+          <p>Copyright &copy; 2022 createaniket</p>
+        </div>
+
+        <div className="righter">
+          <div className="contactfooter">
+            <p id="contactheadingfooter">Contact</p>
+            <p id="phone">
+              {" "}
+              <BsFillTelephoneInboundFill /> +91 6392347125{" "}
+            </p>
+            <p id="mail">
+              {" "}
+              <AiFillMail /> aniketpratapsingh7310@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Wherenext.jsx
+++ src/Components/Wherenext.jsx
@@ -6,9 +6,12 @@ import london from "../Assets/london.jpg";
 import ocean from "../Assets/ocean.jpg";
 import winter from "../Assets/manalimm.jpg";
 import usa from "../Assets/usa.jpg";
+import { Link } from "react-router-dom";
 
 const Wherenext = () => {
+  
   return (
+
     <div className="nesc" id="wherenext">
       <div className="maintext">
         <p>PLAN YOUR TRIP</p>
@@ -17,7 +20,14 @@ const Wherenext = () => {
 
       <div className="container">
         <div class="card" style={{ width: "18rem" }}>
-          <img id="singapore" src={singapore} class="card-img-top" alt="..." />
+          <Link to="/singapore">
+            <img
+              id="singapore"
+              src={singapore}
+              class="card-img-top"
+              alt="..."
+            />
+          </Link>
           <div class="card-body">
             <h5 class="card-text">SINGAPORE</h5>
             <div className="hideshow">Click on the image to explore more</div>
@@ -25,7 +35,9 @@ const Wherenext = () => {
         </div>
 
         <div class="card" style={{ width: "18rem" }}>
-          <img src={london} class="card-img-top" alt="..." />
+          <Link to="/london">
+            <img src={london} class="card-img-top" alt="..." />
+          </Link>
           <div class="card-body">
             <h5 class="card-text"> LONDON</h5>
             <div className="hideshow">Click on the image to explore more</div>
@@ -33,7 +45,9 @@ const Wherenext = () => {
         </div>
 
         <div class="card" style={{ width: "18rem" }}>
-          <img src={maldives} class="card-img-top" alt="..." />
+          <Link to="/maldives">
+            <img src={maldives} class="card-img-top" alt="..." />
+          </Link>
           <div class="card-body">
             <h5 class="card-text"> MALDIVES</h5>
             <div className="hideshow">Click on the image to explore more</div>
@@ -41,7 +55,9 @@ const Wherenext = () => {
         </div>
 
         <div class="card" style={{ width: "18rem" }}>
-          <img src={ocean} class="card-img-top" alt="..." />
+          <Link to="/miami">
+            <img src={ocean} class="card-img-top" alt="..." />
+          </Link>
           <div class="card-body">
             <h5 class="card-text">MIAMI</h5>
             <div className="hideshow">Click on the image to explore more</div>
@@ -49,7 +65,9 @@ const Wherenext = () => {
         </div>
 
         <div class="card" style={{ width: "18rem" }}>
-          <img src={winter} class="card-img-top" alt="..." />
+          <Link to="/manali">
+            <img src={winter} class="card-img-top" alt="..." />
+          </Link>
           <div class="card-body">
             <h5 class="card-text">MANALI</h5>
             <div className="hideshow">Click on the image to explore more</div>
@@ -57,7 +75,9 @@ const Wherenext = () => {
         </div>
 
         <div class="card" style={{ width: "18rem" }}>
-          <img src={usa} class="card-img-top" alt="..." />
+          <Link to="/usa">
+            <img src={usa} class="card-img-top" alt="..." />
+          </Link>
           <div class="card-body">
             <h5 class="card-text">U.S.A</h5>
             <div className="hideshow">Click on the image to explore more</div>
